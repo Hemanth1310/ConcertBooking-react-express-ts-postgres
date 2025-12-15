83ab03ba1ec7c7a8f60311f0c29db244f06091a9
Hemanth1310 <hemanth13k@gmail.com>
2025-12-15 10:56:02 +0100

    backend-public-route-images

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import authRouter from './authenticationRoutes'
 import protectedRoutes from './protectedRoutes'
 import publicRoutes from './publicRoutes'
 import cors from 'cors'
+import path from "path";
 const app = express()
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); 
@@ -11,9 +12,30 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use('/images',express.static("public"))
 app.use('/auth',authRouter)
 app.use('/api',protectedRoutes)
 app.use('/data',publicRoutes)
+
+const publicPath = path.join(__dirname,'..','public')
+
+
+
+app.get('/images/:filename',(req,res)=>{
+    const filename = req.params.filename
+    const imagesPath = path.join(publicPath,filename)
+
+    if(!imagesPath.startsWith(publicPath)){
+        res.status(405).send("Forbidden")
+    }
+
+    res.sendFile(imagesPath,(err)=>{
+        if(err){
+            res.status(404).send("File not found")
+        }
+    })
+})
+
 
 app.listen(3008,()=>{
     console.log("Listening on port 3008")
