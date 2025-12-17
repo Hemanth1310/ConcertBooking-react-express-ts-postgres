25120645117376e338c7b834ec482178b43ebd53
Hemanth1310 <hemanth13k@gmail.com>
2025-12-17 10:40:46 +0100

    frontend-Concert-details-ticket-ifo-display-complete

--- frontend/src/views/ConcertDetails.tsx
+++ frontend/src/views/ConcertDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router'
 import dataProvider from '../utils/dataProvider'
 import getImageUrl from '../utils/getImageUrl'
@@ -10,7 +10,9 @@ const ConcertDetails = (props: Props) => {
     const {name,id} = useParams()
     const {data:concert,isLoading,isError} = useConcertDetails(Number(id))
     const {data:ticketInfo,isLoading:isTicketsLoading,isError:isTicketsError} = useTicketInfo(Number(id))
-      if(isLoading){
+    const [count,setCount] = useState({})
+    
+    if(isLoading){
     return <div>"Page is loading . please wait"</div>
   }
   if(isError){
@@ -21,7 +23,6 @@ const ConcertDetails = (props: Props) => {
         }
     const dateObject = new Date(concert.date)
     
-
    
   
   return (
@@ -49,18 +50,36 @@ const ConcertDetails = (props: Props) => {
             <h1 className='text-2xl md:text-3xl font-bold font-mono py-5'>Tickets Information</h1>
             {isTicketsLoading&&<div>Loading, please wait</div>}
             
-            <div className='flex gap-5'>
+            <div className=''>
                 {ticketInfo?.map(ticket=>{
                     const availabilityPercent = ticket.availableQuantity/ticket.totalQuantity
                 return(
-                <div className='flex-col gap-2 items-center shadow-md'>
-                    
-                    <div className={` p-5 text-white text-2xl rounded-tl-2xl rounded-tr-2xl flex items-center justify-center bg-gray-800`} >{ticket.name}</div>
-                    <div className={`p-5 items-center justify-center  bg-gray-100`}>
-                        <div className='text-2xl '>Availability</div>
-                        <p>{ticket.availableQuantity}</p>
-                        <div className='text-2xl '>Price</div>
-                        <p>${ticket.price}</p>
+                <div className='flex gap-2 items-center rounded-2xl shadow-md justify-between'>
+                     <div className={`flex-1 flex flex-col px-10 py-5  `}>
+                        <div className={`flex-1  text-2xl rounded-tl-2xl rounded-tr-2xl flex items-center text-gray-800`} >{ticket.name}</div>
+                        {availabilityPercent<0.3 && <div className='text-red-600'>Scarsity</div>}
+                        {(availabilityPercent<0.5 && availabilityPercent>0.3)&& <div className='text-orange-400'>Fast Filling</div>}
+                          {availabilityPercent>0.5 && <div className='text-green-600'>Available</div>}
+                    </div>
+                    <div className={`flex-1 flex p-5 items-center justify-between  `}>
+                        <div> 
+                            <div className='text-2xl '>Spots Left</div>
+                            <p>{ticket.availableQuantity}</p>
+                        </div>
+                      
+                       
+                    </div>
+                     <div className={`flex-1 flex p-5 items-center justify-between  `}>
+
+                       <div>
+                            <div className='text-2xl '>Price</div>
+                            <p>${ticket.price}</p>
+                       </div>
+                       
+                    </div>
+                    <div className={`flex p-5 items-center justify-between  `}>
+                        <div className='bg-gray-900 px-5 py-3 rounded-2xl text-white text-2xl'>Book</div>
+
                     </div>
                 </div>)})}
             
