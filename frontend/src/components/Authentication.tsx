3ee97dd0933b86ad65e7c979460b8f666b7aa746
Hemanth1310 <hemanth13k@gmail.com>
2025-12-17 20:57:55 +0100

    frontend-auth-bug-fix

--- frontend/src/components/Authentication.tsx
+++ frontend/src/components/Authentication.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Modal from './Modal'
 import Login from './Login';
 import Register from './Register';
@@ -9,25 +9,30 @@ type Props = {
   closeModal:()=>void
 }
 
-const Authentication = ({isModalOpen,openModal,closeModal}: Props) => {
+const Authentication = ({isModalOpen,closeModal}: Props) => {
     const [toggle,setToggle] = useState<boolean>(false)
+    
     const FormComponent = toggle? Register:Login
+    
     const formKey = toggle ? 'register' : 'login';
     const title = toggle ? 'Register' : 'Login';
-    useEffect(()=>{
+
+    const closeModalHandler=()=>{
         setToggle(false)
-    },[isModalOpen])
+        closeModal()
+    }
+    
   return (
 
                 <Modal 
                     isOpen={isModalOpen} 
-                    onClose={closeModal}
+                    onClose={closeModalHandler}
                     title={title}
                 >
                     {/* Content passed as children */}
                     <div className='w-full flex flex-col items-center gap-5'>
                         <div key={formKey} className='w-full'>
-                            <FormComponent closeModal={closeModal}/>
+                            <FormComponent closeModal={closeModalHandler}/>
                          </div>
                          {toggle?<div>Already Registered? <span className='text-blue-700' onClick={()=>setToggle(!toggle)}>SignIn here</span></div>:
                          <div>Not Registered? <span className='text-blue-700' onClick={()=>setToggle(!toggle)}>SignUp here</span></div>}
