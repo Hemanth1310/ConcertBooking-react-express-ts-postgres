8397b8905a7b3a237eb2150dddb808268c47fdf7
Hemanth1310 <hemanth13k@gmail.com>
2025-12-17 11:12:17 +0100

    frontend-dataProvider-home-formatting

--- frontend/src/utils/dataProvider.ts
+++ frontend/src/utils/dataProvider.ts
@@ -1,43 +1,44 @@
-import { useMemo } from "react"
-import type { Concert, ConcertCategory } from "../types"
-import { useConcerts } from "./hooks/concertDataHook"
+import { useMemo } from "react";
+import type { Concert, ConcertCategory } from "../types";
+import { useConcerts } from "./hooks/concertDataHook";
 
+const dataProvider = () => {
+  const { data: concerts, isLoading, isError } = useConcerts();
 
-const dataProvider = ()=>{
-    const {data:concerts,isLoading,isError} =  useConcerts()
+  const prcessedData = useMemo(() => {
+    const catogorisedData: Record<string, Concert[]> = {};
+    const featuredData: Concert[] = [];
+    const concertNames: string[] = [];
+    const concertCategories: ConcertCategory[] = [];
+    const concertsData: Concert[] = concerts ? concerts : [];
 
-    const prcessedData = useMemo(()=>{
-        const catogorisedData : Record<string , Concert[]> = {}
-    const featuredData : Concert[]  =[]
-    const concertNames : string[] = []
-    const concertCategories: ConcertCategory[] = []
-    const concertsData:Concert[] =concerts?concerts:[]
-    
+    concerts?.forEach((concert) => {
+      if (concert.isFeatured) {
+        featuredData.push(concert);
+      }
 
-    concerts?.forEach((concert)=>{
-        if(concert.isFeatured){
-            featuredData.push(concert)
-        }
-        
+      const category: ConcertCategory = concert.category;
+      if (!catogorisedData[category]) {
+        catogorisedData[category] = [];
+      }
+      if (!concertCategories.includes(category)) {
+        concertCategories.push(category);
+      }
 
-        const category : ConcertCategory = concert.category
-        if(!catogorisedData[category]){
-            catogorisedData[category]=[]
-        }
-        if(!concertCategories.includes(category)){
-            concertCategories.push(category)
-        }
+      catogorisedData[category].push(concert);
+      concertNames.push(concert.name);
+    });
 
-        catogorisedData[category].push(concert)
-        concertNames.push(concert.name)
-        
-    })
+    return {
+      catogorisedData,
+      featuredData,
+      concertNames,
+      concertCategories,
+      concertsData,
+    };
+  }, [concerts]);
 
-    return {catogorisedData,featuredData,concertNames,concertCategories,concertsData}
-    },[concerts])
+  return { ...prcessedData, isLoading, isError };
+};
 
-    return {...prcessedData,isLoading,isError}
-    
-}
-
-export default dataProvider
+export default dataProvider;
