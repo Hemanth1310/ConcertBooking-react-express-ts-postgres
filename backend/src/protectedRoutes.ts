4b313e8950e61d23dc66f824f71b61751e079d2d
Hemanth1310 <hemanth13k@gmail.com>
2025-12-17 14:48:48 +0100

    backend-protected-route-booking

--- backend/src/protectedRoutes.ts
+++ backend/src/protectedRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import authenticateToken from './middleware/auth'
 import { prisma } from './prisma'
+import { Booking, Prisma } from '@prisma/client'
 
 const router = express.Router()
-
-router.get('/userDetails',authenticateToken,async(req,res)=>{
+router.use(authenticateToken)
+router.get('/userDetails',async(req,res)=>{
     const userId = req.user?.userId
 
     try{
@@ -25,6 +26,60 @@ router.get('/userDetails',authenticateToken,async(req,res)=>{
     }catch(error){
         res.status(404).json({error:'User not found'})
     }
+})
+
+router.post('/booking/:concertId/:ticketTypeId',async(req,res)=>{
+    const {concertId,ticketTypeId} = req.params;
+    const userId=req.user?.userId
+    const {qty} = req.query
+    const quantity = Number(qty)
+    const formatedTicketTypeId = Number(ticketTypeId)
+  
+
+    try{
+
+        const currentTicketInfo = await prisma.ticketType.findUnique({
+            where:{id:formatedTicketTypeId},
+        })
+        if(currentTicketInfo && userId){
+            
+            const newAvailableQuantity = currentTicketInfo.availableQuantity - quantity
+            const totalPrice= currentTicketInfo.price * quantity
+            const newBooking :Booking=await prisma.booking.create({
+                data:{
+                    quantity:newAvailableQuantity,
+                    totalPrice:totalPrice,
+                    user: {
+                        connect: { id: userId } 
+                    },
+                    ticketType: {
+                        connect: { id: formatedTicketTypeId }
+                    },
+                }
+            })
+            await prisma.ticketType.update({
+                data:{
+                    availableQuantity:newAvailableQuantity
+                },
+                where:{id:formatedTicketTypeId}
+            }) 
+
+            res.json({
+                message:"Booking Successful",
+                payload:{
+                    ...newBooking
+                }
+            })
+            
+        }else{
+            res.status(400).send("Requested ticket not available")
+        }
+        
+
+    }catch(error){
+        res.status(405).send(error)
+    }
+
 })
 
 export default router 
