69f1580741920bbfad1310bf6e6ef090e66636f2
Hemanth1310 <hemanth13k@gmail.com>
2025-12-22 11:04:03 +0100

    backend-user-profile-image

--- frontend/src/views/Profile.tsx
+++ frontend/src/views/Profile.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import { useAuth } from '../context/AuthContext'
+import Spinner from '../components/Spinner'
 
 
 const Profile = () => {
+    const {userData,isAuthLoading} = useAuth()
+
+    if(isAuthLoading){
+        <div className="w-full h-screen flex font-mono italic text-gray-500 items-center justify-center text-3xl">
+            <Spinner/>
+        </div>
+    }
   return (
      <div className="flex px-5 md:px-0 flex-col w-full h-full mt-5">
-      <h1 className="text-2xl md:text-3xl font-bold font-mono py-5">
-        User Details
-      </h1>
-      <div>
-        
-      </div>
+        <div>
+             <h1 className="text-2xl md:text-3xl font-bold font-mono py-5">
+                Account Details
+            </h1>
+            <div>
+
+            </div>
+        </div>
+     
 
       </div>
   )
