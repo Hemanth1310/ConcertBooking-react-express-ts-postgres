f12bc682988c2de7bf38e12a0cb7ebc8a386a27e
Hemanth1310 <hemanth13k@gmail.com>
2025-12-25 16:52:09 +0100

    frontend-Password-reset-validation

--- frontend/src/views/PasswordReset.tsx
+++ frontend/src/views/PasswordReset.tsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import api from '../utils/axiosConfig'
+import z from 'zod'
 
 const PasswordReset = () => {
+    const [isValidated,setValidated] = useState(false)
+    const emailRef = useRef<HTMLInputElement>(null)
+
+
+    const handleValidation = async () => {
+    const email = emailRef.current?.value;
+
+    const isFormatValid = z.string().email().safeParse(email).success;
+    if (!isFormatValid) return;
+
+    try {
+        const { data } = await api.get(`/data/validate?email=${email}`);
+        if (data.isValid === 'userfound') {
+            setValidated(true);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
   return (
-    <div>PasswordReset</div>
+     <div className="flex px-5 md:px-0 flex-col w-full h-full mt-5 items-center justify-center">
+        <div className='w-full md:w-1/2 py-10 border-2 px-5 flex flex-col items-center'>
+             <h1 className="text-2xl md:text-3xl font-bold font-mono py-5">
+                Password Reset
+            </h1>
+                {!isValidated? <div className='w-full flex flex-col md:flex-row  gap-5'>
+                                    <div className='flex flex-col gap-2 flex-1'>
+                                        <span className='text-xl px-2'>Email</span>
+                                        <input ref={emailRef}  name='email' className='border-2 rounded-2xl border-gray-400 text-xl p-4 w-full' placeholder='Enter your email'></input>
+                                        <button onClick={handleValidation} className='bg-brand w-50 text-white text-xl p-3 rounded-2xl hover:opacity-75'>Save Changes</button>
+                                    </div>   
+                    </div>:<div className='w-full flex flex-col md:flex-row  gap-5'>
+                                    <div className='flex flex-col gap-2 flex-1'>
+                                        <span className='text-xl px-2'>Firstname</span>
+                                        <input  name='firstName' className='border-2 rounded-2xl border-gray-400 text-xl p-4 w-full' ></input>
+                                    </div>
+                                    <div className='flex flex-col gap-2 flex-1'>
+                                        <span className='text-xl px-2'>Lastname</span>
+                                        <input  name='lastName' className='border-2 rounded-2xl border-gray-400 text-xl p-4' ></input>
+                                    </div>
+
+                    </div>}
+                   
+
+        </div>
+    </div>
   )
 }
 
