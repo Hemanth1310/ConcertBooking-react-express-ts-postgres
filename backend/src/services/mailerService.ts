76aa9b59ec5a4533df900fd88dd68b0c8bd1c94f
Hemanth1310 <hemanth13k@gmail.com>
2026-01-13 17:16:36 +0100

    backend-module-sendVerification-2

--- backend/src/services/mailerService.ts
+++ backend/src/services/mailerService.ts
@@ -13,6 +13,17 @@ const sender = {
   name: process.env.EMAIL_NAME,
 };
 
-export const sendVerificationEmail =async (userEMAIL:string,userID:string) =>{
-    
+export const sendVerificationEmail =async (userEMAIL:string,userId:string) =>{
+    const token = jwt.sign({ userId }, process.env.JWT_VERIFICATION_SECRET!, { expiresIn: '1d' });
+    const verificationEmail= `${process.env.FRONTEND_URL}/verify-email?token=${token}`
+
+transport
+  .sendMail({
+    from: sender,
+    to: userEMAIL,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
 }
