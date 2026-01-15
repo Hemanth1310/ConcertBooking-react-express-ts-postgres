c29217d05f763bdffb075822cf36377c9f533263
Hemanth1310 <hemanth13k@gmail.com>
2026-01-15 16:14:09 +0100

    frontend-VErifyEmail-css-format

--- frontend/src/views/VerifyEmail.tsx
+++ frontend/src/views/VerifyEmail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate, useSearchParams } from "react-router";
@@ -17,18 +17,26 @@ const VerifyEmail = () => {
         return;
       }
       try {
-        await axios.get(`http://localhost:3008/auth/verify-email?token=${token}`);
-        setIsVerifying(false);
-        setIsVerified(true);
-        setTimeout(() => {
-          navigate("/");
-        }, 3000);
+        const { data } = await axios.get(
+          `http://localhost:3008/auth/verify-email?token=${token}`
+        );
+        if (data.status) {
+          setIsVerifying(false);
+          setIsVerified(true);
+          setTimeout(() => {
+            navigate("/");
+          }, 3000);
+        } else {
+          setIsVerifying(false);
+          throw new Error("Unable to validate user.");
+        }
       } catch (error) {
+        setIsVerifying(false);
         console.log(error);
       }
     };
     verifyEmail();
-  }, [ navigate, searchParams]);
+  }, [navigate, searchParams]);
 
   if (isVerifying) {
     return (
@@ -39,17 +47,21 @@ const VerifyEmail = () => {
   }
 
   return (
-    <div className="flex px-5 md:px-0 flex-col w-full h-full mt-5">
-      <h1 className="text-2xl md:text-3xl font-bold font-mono py-5">
-        Verifying your email
-      </h1>
+    <div className="w-full h-screen flex items-center justify-center px-5 md:px-0 flex-col mt-5">
       <div className="flex flex-col gap-5">
         {isVerified ? (
-          <div>Your Email is verified</div>
+          <div className="text-center">
+            <h1 className="text-2xl md:text-3xl font-bold font-mono py-5">
+              ✅ Your email verification completed 
+            </h1>
+            Your view will be automatically redirected to home page.
+          </div>
         ) : (
-          <div>
-            Your email was not verified please contact us at
-            service@concertzz.com
+          <div className="text-center">
+            <h1 className="text-2xl md:text-3xl font-bold font-mono py-5">
+               ❌ Your email verification failed
+            </h1>
+            Please contact us at service@concertzz.com
           </div>
         )}
       </div>
